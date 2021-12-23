99fb73c546a2ad7cbd3fde27c9868666377346c6
jc denton777d <vapemonk@outlook.com>
2021-12-23 14:33:58 -0800

    upd from gitlab work today

---
+++ joyjitteryrectangles-main-main/validintRg.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# validing--Validates integer input, allowing negative integers too
+
+validint()
+{
+    # Validate first fields and test that value against min value $2 and/or
+    #  max value $3 if they are supplied.  If the value isn't within range
+    #  or it's not composed of just digits, fail.
+
+    number
+}
