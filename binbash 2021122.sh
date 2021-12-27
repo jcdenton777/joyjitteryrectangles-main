3cd606781b49d22111b8b4f6174be08626f70685
jc denton777d <vapemonk@outlook.com>
2021-12-27 10:27:48 -0800

    ljlkjkl

---
+++ binbash 2021122.sh
@@ -0,0 +1,8 @@
+#!/bin/bash/
+# Lesson Script #3 - N# normdate--Normalizes month field in
+# date specification to three letters,
+# first letter capitalized. A helper function for Script #7, 
+# valid-date.
+# Exits with 0 if no error.
+
+monthNumToName
